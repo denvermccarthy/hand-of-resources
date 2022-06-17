e5849b8af0c8c6f048583bb5a088f9ebfc00a609
Denver McCarthy <dmichaelspdx@gmail.com>
2022-06-17 10:35:52 -0700

    finished /books route

--- __tests__/book.test.js
+++ __tests__/book.test.js
@@ -7,9 +7,16 @@ describe('testing for /book route', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('A GET request to /books should return a list of books', () => {
-    const req = request(app).get('/books');
+  it('A GET request to /books should return a list of books', async () => {
+    const req = await request(app).get('/books');
     expect(req.status).toEqual(200);
+    expect(req.body.length).toEqual(5);
+    const harryPotterBook = req.body.find((book) => book.id === '1');
+    expect(harryPotterBook).toMatchObject({
+      id: '1',
+      title: 'Harry Potter and the Philosophers Stone',
+      released: 1997,
+    });
   });
   afterAll(() => {
     pool.end();

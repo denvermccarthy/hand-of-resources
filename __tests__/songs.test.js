7a5ff428d27fc23e3f05321010ba837c29e942d9
Denver McCarthy <dmichaelspdx@gmail.com>
2022-06-26 23:39:22 -0700

    added get route for songs

---
+++ __tests__/songs.test.js
@@ -0,0 +1,20 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('testing for songs', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  test('/songs should return a list of songs', async () => {
+    const res = await request(app).get('/songs');
+    expect(res.status).toEqual(200);
+    const mtrees = res.body.find((song) => song.title === 'Money Trees');
+    expect(mtrees.minutes).toEqual(6);
+    expect(res.body.length > 1).toEqual(true);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});

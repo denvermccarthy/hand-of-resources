7dc1246994e0dd5822e0d3d9993e025b47b67417
Denver McCarthy <dmichaelspdx@gmail.com>
2022-07-09 22:39:09 -0700

    added post route for songs

--- __tests__/songs.test.js
+++ __tests__/songs.test.js
@@ -7,18 +7,30 @@ describe('testing for songs', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  test('/songs should return a list of songs', async () => {
+  test('GET to /songs should return a list of songs', async () => {
     const res = await request(app).get('/songs');
     expect(res.status).toEqual(200);
     const mtrees = res.body.find((song) => song.title === 'Money Trees');
     expect(mtrees.minutes).toEqual(6);
     expect(res.body.length > 1).toEqual(true);
   });
-  test('/songs/id should send the correct artist', async () => {
+  test('GET to /songs/id should send the correct song', async () => {
     const res = await request(app).get('/songs/2');
     expect(res.status).toEqual(200);
     expect(res.body.title).toEqual('Backseat Freestyle');
     expect(res.body.minutes).toEqual(3);
+  });
+  test('Post to /songs should add a song', async () => {
+    const songData = {
+      title: 'Total Football',
+      minutes: 4,
+    };
+    const res = await request(app).post('/songs').send(songData);
+    expect(res.status).toEqual(200);
+    expect(res.body).toEqual({
+      ...songData,
+      id: expect.any(String),
+    });
   });
   afterAll(() => {
     pool.end();

3e2272e04f19b31aea69c96a3e856a939a3a95a8
Denver McCarthy <dmichaelspdx@gmail.com>
2022-07-10 11:29:21 -0700

    added detail route to movies

--- __tests__/movies.test.js
+++ __tests__/movies.test.js
@@ -8,10 +8,22 @@ describe('testing for songs', () => {
     return setup(pool);
   });
   test('get to /movies should return a list of movies', async () => {
-    const { body: movies } = await request(app).get('/movies');
+    const resp = await request(app).get('/movies');
+    const { body: movies } = resp;
+    expect(resp.status).toBe(200);
     expect(movies.length > 1).toBe(true);
     expect(movies[0]).toEqual({
       id: expect.any(String),
+      title: expect.any(String),
+      rating: expect.any(Number),
+    });
+  });
+  test('get to /movies/id should return a movie', async () => {
+    const resp = await request(app).get('/movies/1');
+    const { body: movies } = resp;
+    expect(resp.status).toBe(200);
+    expect(movies).toEqual({
+      id: '1',
       title: expect.any(String),
       rating: expect.any(Number),
     });
